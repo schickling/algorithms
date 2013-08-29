1e3a9275532c7a99c50e0d073e176d688b803284
Johannes Schickling <schickling.j@gmail.com>
2013-08-29 22:02:27 +0200

    Added description

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,18 +5,23 @@ angular.module('algorithmsApp')
     $scope.algorithms = [
       {
         name: 'Chinese remainder theorem',
+        description: 'Solves a system of congruence equations',
         url: 'chinese-remainder-theorem',
       }, {
         name: 'Extended euclidean algorithm',
+        description: 'Calculates the greates common divisor',
         url: 'extended-euclidean-algorithm',
       }, {
         name: 'Gaussian elimination',
+        description: 'Row reduces a matrix',
         url: 'gaussian-elimination',
       }, {
         name: 'Invert matrix',
+        description: 'Inverts a matrix with the gaussian elimination',
         url: 'invert-matrix',
       }, {
         name: 'Smith normal form',
+        description: 'Translate a matrix smith normal form',
         url: 'smith-normal-form',
       },
     ];
