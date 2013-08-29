050ef23c87bc2a7fb60eafe5e557c06c80c37b6b
Johannes Schickling <schickling.j@gmail.com>
2013-08-29 21:49:17 +0200

    pushstate urls

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('algorithmsApp', ['ngRoute'])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
