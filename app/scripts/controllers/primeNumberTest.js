06b61ab8276556f4469c40a00805223cbe47475d
Johannes Schickling <schickling.j@gmail.com>
2013-08-31 13:43:37 +0200

    fixed syntax

--- app/scripts/controllers/primeNumberTest.js
+++ app/scripts/controllers/primeNumberTest.js
@@ -2,11 +2,12 @@
 
 angular.module('algorithmsApp')
 	.controller('PrimeNumberTest', function ($scope) {
+		
 		$scope.number = 23;
 
 		$scope.calculate = function() {
 			$scope.isPrime = $scope.number.isPrime();
-		}
+		};
 
 		$scope.calculate();
 	});
