8f90cd6796f39c781539855588c2c38c201d34e1
Johannes Schickling <schickling.j@gmail.com>
2013-09-03 11:43:18 +0200

    Added view

--- app/scripts/controllers/primeNumberGenerators.js
+++ app/scripts/controllers/primeNumberGenerators.js
@@ -1,6 +1,24 @@
 'use strict';
 
 angular.module('algorithmsApp')
-	.controller('primeNumberGenerators', function ($scope) {
-		
+	.controller('primeNumberGenerators', function ($scope, PrimeNumberGenerator) {
+
+
+		$scope.algorithms = [
+			{
+				name: 'Primorial calculation',
+				method: PrimeNumberGenerator.primorial
+			}
+		];
+
+		$scope.selectedAlgorithm = $scope.algorithms[0];
+
+		$scope.amount = 10;
+
+		$scope.calculate = function() {
+			$scope.primeNumbers = $scope.selectedAlgorithm.method($scope.amount);
+		};
+
+		$scope.calculate();
+
 	});
