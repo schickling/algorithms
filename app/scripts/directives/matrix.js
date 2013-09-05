71cdac892443455dd0976024296c945557a452d1
Johannes Schickling <schickling.j@gmail.com>
2013-09-05 21:17:09 +0200

    Added matrix directive

---
+++ app/scripts/directives/matrix.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('algorithmsApp')
+	.directive('matrix', function () {
+		return {
+			templateUrl: 'scripts/directives/templates/matrix.html',
+			restrict: 'E',
+			scope: true,
+			link: function postLink(scope, element, attrs) {
+				scope.matrix = scope.$parent[attrs.ngModel];
+				scope.showDelete = attrs.ngShowDelete || false;
+			}
+		};
+	});
