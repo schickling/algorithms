7c0ac25b075d69fb97fc6ffcada4494f4407303a
Johannes Schickling <schickling.j@gmail.com>
2013-09-10 15:31:24 +0200

    Implemented solving

--- app/scripts/services/lUDecomposition.js
+++ app/scripts/services/lUDecomposition.js
@@ -8,25 +8,20 @@ angular.module('algorithmsApp')
 			calculate: function (A, b, usePivoting) {
 
 				var m = A.length,
-					L = Utils.identityMatrix(m),
+					L = Utils.emptyMatrix(m),
 					U = A.clone(),
 					P = Utils.identityMatrix(m),
-					x = [1],
-					y = [1],
-					currentRow, pivotRow, rowToKill, column, factor, maximumValue, maximumRow, pivotValue, partialP;
+					x = [],
+					y = [],
+					currentRow, pivotRow, rowToKill, column, factor, maximumValue, maximumRow, pivotValue, partialP, sum;
 
-				if (usePivoting) {
-
-				}
-
+				// factorize A = L * U
 				for (currentRow = 0; currentRow < m; currentRow++) {
 
 					if (usePivoting) {
 
 						// determinte row with maximum pivot element
-						maximumValue = 0;
-						maximumRow = currentRow;
-						for (pivotRow = currentRow; pivotRow < m; pivotRow++) {
+						for (maximumValue = 0, maximumRow = currentRow, pivotRow = currentRow; pivotRow < m; pivotRow++) {
 
 							pivotValue = Math.abs(U[pivotRow][currentRow]);
 
@@ -45,18 +40,52 @@ angular.module('algorithmsApp')
 
 						P = Utils.matrixMultiply(partialP, P);
 						U = Utils.matrixMultiply(partialP, U);
+						L = Utils.matrixMultiply(partialP, L);
 
 					}
+
+					L[currentRow][currentRow] = 1;
 
 					for (rowToKill = currentRow + 1; rowToKill < m; rowToKill++) {
 
 						factor = U[rowToKill][currentRow] / U[currentRow][currentRow];
-						L[rowToKill][currentRow] = -factor;
+						L[rowToKill][currentRow] = factor;
 
 						for (column = currentRow; column < m; column++) {
 							U[rowToKill][column] -= U[currentRow][column] * factor;
 						}
 					}
+				}
+
+				// adjust b
+				if (usePivoting) {
+					b = b.map(function (element) {
+						return [element]
+					});
+					b = Utils.matrixMultiply(P, b);
+					b = b.map(function (element) {
+						return element[0];
+					});
+				}
+
+				// forward substitute L * y = b
+				for (currentRow = 0; currentRow < m; currentRow++) {
+
+					for (sum = 0, column = 0; column < currentRow; column++) {
+						sum += L[currentRow][column] * y[column];
+					}
+
+					y[currentRow] = b[currentRow] - sum;
+				}
+
+				// backward substitute U * x = y
+				for (currentRow = m - 1; currentRow >= 0; currentRow--) {
+
+					for (sum = 0, column = currentRow + 1; column < m; column++) {
+						sum += U[currentRow][column] * x[column];
+					}
+
+					x[currentRow] = (y[currentRow] - sum) / U[currentRow][currentRow];
 				}
 
 				return {
--- test/spec/services/lUDecomposition.js
+++ test/spec/services/lUDecomposition.js
@@ -12,30 +12,30 @@ describe('Service: lUDecomposition', function () {
   }));
 
   it('should do decomposite 3*3 matrix without pivoting', function () {
-    var result = lUDecomposition.calculate([[1, 2, 1], [2, 2, 3], [3, 5, 4]], [1, 1, 1], false),
-      expectedX = [1],
-      expectedY = [1],
-      expectedP = [[1]],
-      expectedL = [[1, 0, 0], [-2, 1, 0], [-3, -0.5, 1]],
+    var result = lUDecomposition.calculate([[1, 2, 1], [2, 2, 3], [3, 5, 4]], [4, 2, 5], false),
+      expectedX = [14, -1, -8],
+      expectedY = [4, -6, -4],
+      expectedP = [[1, 0, 0], [0, 1, 0], [0, 0, 1]],
+      expectedL = [[1, 0, 0], [2, 1, 0], [3, 0.5, 1]],
       expectedU = [[1, 2, 1], [0, -2, 1], [0, 0, 0.5]];
 
-    // expect(result.x).toEqual(expectedX);
-    // expect(result.y).toEqual(expectedY);
-    // expect(result.P).toEqual(expectedP);
+    expect(result.x).toEqual(expectedX);
+    expect(result.y).toEqual(expectedY);
+    expect(result.P).toEqual(expectedP);
     expect(result.L).toEqual(expectedL);
     expect(result.U).toEqual(expectedU);
   });
 
   it('should do decomposite 3*3 matrix with pivoting', function () {
-    var result = lUDecomposition.calculate([[-1, 2, 3], [-2, 7, 4], [1, 4, -2]], [1, 1, 1], true),
-      expectedX = [1],
-      expectedY = [1],
+    var result = lUDecomposition.calculate([[-1, 2, 3], [-2, 7, 4], [1, 4, -2]], [1, 2, 3], true),
+      expectedX = [2.466666666666667, 0.5333333333333333, 0.8],
+      expectedY = [2, 4, 0.8],
       expectedP = [[0, 1, 0], [0, 0, 1], [1, 0, 0]],
-      expectedL = [[1, 0, 0], [-0.5, 1, 0], [0.5, 0.2, 1]],
+      expectedL = [[1, 0, 0], [-0.5, 1, 0], [0.5, -0.2, 1]],
       expectedU = [[-2, 7, 4], [0, 7.5, 0], [0, 0, 1]];
 
-    // expect(result.x).toEqual(expectedX);
-    // expect(result.y).toEqual(expectedY);
+    expect(result.x).toEqual(expectedX);
+    expect(result.y).toEqual(expectedY);
     expect(result.P).toEqual(expectedP);
     expect(result.L).toEqual(expectedL);
     expect(result.U).toEqual(expectedU);
