1d1d8344d693023e0d7af3e8c5e30311fb120c49
Johannes Schickling <schickling.j@gmail.com>
2013-09-10 22:35:45 +0200

    Fixed buttons

--- app/scripts/controllers/lUDecomposition.js
+++ app/scripts/controllers/lUDecomposition.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('algorithmsApp')
-	.controller('lUDecomposition', function ($scope, LUDecomposition) {
+	.controller('lUDecomposition', function ($scope, LUDecomposition, Utils) {
 
 		$scope.usePivoting = false;
 		$scope.A = [[1, 2, 1], [2, 2, 3], [3, 5, 4]];
@@ -19,6 +19,19 @@ angular.module('algorithmsApp')
 			$scope.x = result.x;
 			$scope.y = result.y;
 		};
+
+		$scope.makeBigger = function () {
+			Utils.matrixExpand($scope.A);
+			$scope.b.push([Utils.randomNumber(-5, 10)]);
+			$scope.calculate();
+		};
+
+		$scope.makeSmaller = function () {
+			Utils.matrixShrink($scope.A);
+			$scope.b.pop();
+			$scope.calculate();
+		};
+
 
 		$scope.calculate();
 
