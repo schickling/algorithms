f87f9a52e33bbb778402699ecb5e589aa588ed27
Johannes Schickling <schickling.j@gmail.com>
2013-09-10 12:34:59 +0200

    Implemented non pivot version of LU decomposition

--- app/scripts/services/lUDecomposition.js
+++ app/scripts/services/lUDecomposition.js
@@ -1,6 +1,45 @@
 'use strict';
 
 angular.module('algorithmsApp')
-  .service('LUDecomposition', function LUDecomposition() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+	.service('LUDecomposition', function LUDecomposition(Utils) {
+
+		return {
+
+			calculate: function (A, b, usePivoting) {
+
+				var m = A.length,
+					L = Utils.identityMatrix(m),
+					U = A.clone(),
+					P = Utils.identityMatrix(m),
+					x = [1],
+					y = [1],
+					currentRow, pivotRow, column, factor;
+
+				if (usePivoting) {
+
+				}
+
+				for (currentRow = 0; currentRow < m; currentRow++) {
+					for (pivotRow = currentRow + 1; pivotRow < m; pivotRow++) {
+
+						factor = U[pivotRow][currentRow] / U[currentRow][currentRow];
+						L[pivotRow][currentRow] = -factor;
+
+						for (column = currentRow; column < m; column++) {
+							U[pivotRow][column] -= U[currentRow][column] * factor;
+						}
+					}
+				}
+
+				return {
+					L: L,
+					U: U,
+					P: P,
+					x: x,
+					y: y,
+				};
+			}
+
+		};
+
+	});
--- test/spec/services/lUDecomposition.js
+++ test/spec/services/lUDecomposition.js
@@ -11,8 +11,19 @@ describe('Service: lUDecomposition', function () {
     lUDecomposition = LUDecomposition;
   }));
 
-  it('should do something', function () {
-    expect(!!lUDecomposition).toBe(true);
+  it('should do decomposite 3*3 matrix without pivoting', function () {
+    var result = lUDecomposition.calculate([[1, 2, 1], [2, 2, 3], [3, 5, 4]]),
+      expectedX = [1],
+      expectedY = [1],
+      expectedP = [[1]],
+      expectedL = [[1, 0, 0], [-2, 1, 0], [-3, -0.5, 1]],
+      expectedU = [[1, 2, 1], [0, -2, 1], [0, 0, 0.5]];
+
+    // expect(result.x).toEqual(expectedX);
+    // expect(result.y).toEqual(expectedY);
+    // expect(result.P).toEqual(expectedP);
+    expect(result.L).toEqual(expectedL);
+    expect(result.U).toEqual(expectedU);
   });
 
 });
