f1c530a3e6d7e2a62a13f15d6ec31397da392a23
Johannes Schickling <schickling.j@gmail.com>
2013-09-11 20:50:32 +0200

    check for cholesky

--- app/scripts/services/choleskyDecomposition.js
+++ app/scripts/services/choleskyDecomposition.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('algorithmsApp')
-	.service('CholeskyDecomposition', function CholeskyDecomposition() {
+	.service('CholeskyDecomposition', function CholeskyDecomposition(LeibnizDeterminant) {
 
 		return {
 
@@ -13,12 +13,43 @@ angular.module('algorithmsApp')
 
 			},
 
-			_isSymetric: function () {
+			_isSymetric: function (A) {
 
+				for (var rowIndex = 0; rowIndex < A.length; rowIndex++) {
+					for (var columnIndex = rowIndex; columnIndex < A.length; columnIndex++) {
+						if (A[rowIndex][columnIndex] !== A[columnIndex][rowIndex]) {
+							return false;
+						}
+					}
+				}
+
+				return true;
 			},
 
-			_isPositiveDefinite: function () {
+			_isPositiveDefinite: function (A) {
 
+				for (var step = 0; step < A.length; step++) {
+					if (LeibnizDeterminant.calculate(this._getMinorMatrix(A, step)) <= 0) {
+						return false;
+					}
+				}
+
+				return true;
+			},
+
+			_getMinorMatrix: function (A, step) {
+
+				var minor = A.clone(),
+					amountToPop = A.length - step - 1;
+
+				for (var i = 0; i < amountToPop; i++) {
+					for (var j = 0; j < amountToPop; j++) {
+						minor[j].pop();
+					}
+					minor.pop();
+				}
+
+				return minor;
 			}
 
 		};
--- test/spec/services/choleskyDecomposition.js
+++ test/spec/services/choleskyDecomposition.js
@@ -11,8 +11,29 @@ describe('Service: choleskyDecomposition', function () {
     choleskyDecomposition = CholeskyDecomposition;
   }));
 
-  it('should do something', function () {
-    expect( !! choleskyDecomposition).toBe(true);
+  // it('should decomposite 3*3 matrix', function () {
+  //   var result = choleskyDecomposition.calculate([[4, 2, 2], [2, 17, 5], [2, 5, 11]]),
+  //     expectedL = [[2, 0, 0], [1, 4, 0], [1, 1, 3]],
+  //     expectedLT = [[2, 1, 1], [0, 4, 1], [0, 0, 3]];
+
+  //   expect(result.L).toEqual(expectedL);
+  //   expect(result.LT).toEqual(expectedLT);
+  // });
+
+  it('shouldn\'t decomposite non symetric 3*3 matrix', function () {
+    expect(choleskyDecomposition.calculate([[4, 2, 2], [1, 17, 5], [2, 5, 11]])).toBe(false);
+  });
+
+  it('shouldn\'t decomposite non positive definite 3*3 matrix', function () {
+    expect(choleskyDecomposition.calculate([[-1, 2, 2], [2, 17, 5], [2, 5, 11]])).toBe(false);
+  });
+
+  it('shouldn\'t decomposite non positive definite 3*3 matrix', function () {
+    expect(choleskyDecomposition.calculate([[0, 2, 2], [2, 17, 5], [2, 5, 11]])).toBe(false);
+  });
+
+  it('shouldn\'t decomposite non positive definite 2*2 matrix', function () {
+    expect(choleskyDecomposition.calculate([[2, 4], [4, 1]])).toBe(false);
   });
 
 });
