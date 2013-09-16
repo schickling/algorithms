0b9d053421d89110a80aa392a6ee569214be3d47
Johannes Schickling <schickling.j@gmail.com>
2013-09-16 16:31:38 +0200

    Added view

--- app/scripts/controllers/fixed-point-iteration.js
+++ app/scripts/controllers/fixed-point-iteration.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('algorithmsApp')
-  .controller('FixedPointIterationCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('FixedPointIterationCtrl', function ($scope, FixedPointIteration) {
+
+		$scope.functionString = '';
+		$scope.startValue = 1;
+		$scope.iterationSteps = 50;
+
+		$scope.calculate = function( ) {
+			$scope.values = FixedPointIteration.calculate($scope.functionString, $scope.iterationSteps, $scope.startValue);
+		};
+
+	});
