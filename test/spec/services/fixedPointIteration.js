843ea363cf431713c4abfec1378fe4be89f6567d
Johannes Schickling <schickling.j@gmail.com>
2013-09-16 14:57:48 +0200

    Added sin test case

--- test/spec/services/fixedPointIteration.js
+++ test/spec/services/fixedPointIteration.js
@@ -15,4 +15,12 @@ describe('Service: fixedPointIteration', function () {
     expect(fixedPointIteration.calculate('x', 5, 2)).toEqual([2, 2, 2, 2, 2]);
   });
 
+  it('should calculate first 5 iteration steps', function () {
+    expect(fixedPointIteration.calculate('x^2', 5, 2)).toEqual([4, 16, 256, 65536, 4294967296]);
+  });
+
+  it('should calculate first 5 iteration steps', function () {
+    expect(fixedPointIteration.calculate('sin(x)', 5, 0)).toEqual([0, 0, 0, 0, 0]);
+  });
+
 });
