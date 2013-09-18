93a691d771fe90faf11507174237a0713fda059a
Suchanek <d060165@wdfm00277326a.fritz.box>
2013-09-18 13:56:04 +0200

    Added another Test Case

--- test/spec/services/linearRegression.js
+++ test/spec/services/linearRegression.js
@@ -1,17 +1,17 @@
 'use strict';
 
-describe('Service: linearRegression', function () {
+describe('Service: linearRegression', function() {
 
   // load the service's module
   beforeEach(module('algorithmsApp'));
 
   // instantiate service
   var linearRegression;
-  beforeEach(inject(function (LinearRegression) {
+  beforeEach(inject(function(LinearRegression) {
     linearRegression = LinearRegression;
   }));
 
-  it('should fit', function () {
+  it('should calculate m = 1, t = 0 ', function() {
     expect(linearRegression.calculate([{
       x: 0,
       y: 0
@@ -22,6 +22,31 @@ describe('Service: linearRegression', function () {
       m: 1,
       t: 0
     });
+  });
+
+  it('should calculate a more complex example', function() {
+    var inputArray = [{
+      x: 1,
+      y: 2
+    }, {
+      x: 2,
+      y: 8
+    }, {
+      x: 3,
+      y: 16
+    }, {
+      x: 4,
+      y: 6
+    }, {
+      x: 5,
+      y: 6
+    }];
+
+    expect(linearRegression.calculate(inputArray))
+      .toEqual({
+	m: 0.6,
+	t: 5.8
+      });
   });
 
 });
