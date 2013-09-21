9b49e590f5a3848196fa38ebbca3806571440a0e
Johannes Schickling <schickling.j@gmail.com>
2013-09-21 13:09:11 +0200

    Implemented Ctrl

--- app/scripts/controllers/newton-method.js
+++ app/scripts/controllers/newton-method.js
@@ -1,8 +1,17 @@
 'use strict';
 
 angular.module('algorithmsApp')
-	.controller('NewtonMethodCtrl', function ($scope) {
+	.controller('NewtonMethodCtrl', function ($scope, NewtonMethod) {
 
-		
+		$scope.functionString = 'x^2';
+		$scope.derivedFunctionString = '2*x';
+		$scope.startValue = 1;
+		$scope.iterationSteps = 50;
+
+		$scope.calculate = function () {
+			$scope.values = NewtonMethod.calculate($scope.functionString, $scope.derivedFunctionString, $scope.startValue, $scope.iterationSteps);
+		};
+
+		$scope.calculate();
 
 	});
