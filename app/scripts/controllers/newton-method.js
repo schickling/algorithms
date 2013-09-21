26362101a90c0ef53dab973cf8c0e262e7c8384b
Johannes Schickling <schickling.j@gmail.com>
2013-09-21 21:49:26 +0200

    Added newton method

--- app/scripts/controllers/newton-method.js
+++ app/scripts/controllers/newton-method.js
@@ -6,12 +6,31 @@ angular.module('algorithmsApp')
 		$scope.functionString = 'x^2';
 		$scope.derivedFunctionString = '2*x';
 		$scope.startValue = 1;
-		$scope.iterationSteps = 50;
+		$scope.iterationSteps = 5;
 
-		$scope.calculate = function () {
-			$scope.values = NewtonMethod.calculate($scope.functionString, $scope.derivedFunctionString, $scope.startValue, $scope.iterationSteps);
+		$scope.coordinate = function (coordinate) {
+
+			$scope.calculate = function () {
+				var scope = {},
+					node = window.math.parse($scope.functionString, scope),
+					values = NewtonMethod.calculate($scope.functionString, $scope.derivedFunctionString, $scope.startValue, $scope.iterationSteps),
+					m, t, y1, x1, x2;
+
+				values.unshift($scope.startValue);
+
+				for (var i = 1; i < values.length; i++) {
+					x1 = scope.x = values[i - 1];
+					x2 = values[i];
+					y1 = node.eval();
+					m = - y1 / (x2 - x1);
+					t = -y1 - m * x1;
+					coordinate.addFunction(m + '*x + ' + t);
+				}
+			};
+
+			$scope.calculate();
 		};
 
-		$scope.calculate();
+
 
 	});
