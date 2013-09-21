579b14aaee555c55de76628e4d5252db9eaff90b
Johannes Schickling <schickling.j@gmail.com>
2013-09-21 13:08:40 +0200

    add another test

--- test/spec/services/newtonMethod.js
+++ test/spec/services/newtonMethod.js
@@ -11,8 +11,12 @@ describe('Service: newtonMethod', function () {
     newtonMethod = NewtonMethod;
   }));
 
-  it('should calculate first 3 values', function () {
+  it('should calculate first 3 values of linear function', function () {
     expect(newtonMethod.calculate('x', '1', 1, 3)).toEqual([0, 0, 0]);
+  });
+
+  it('should calculate first 3 values of quadratic function', function () {
+    expect(newtonMethod.calculate('x^2', '2*x', 1, 3)).toEqual([0.5, 0.25, 0.125]);
   });
 
 });
